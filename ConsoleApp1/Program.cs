512cdba4d98ee82aaeac965936cbbf8e9b9593b6
visualstudio <visualstudio@230-PC19>
2019-01-31 14:37:21 -0500

    2:377777

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+        }
+        class Village
+        {
+            public static int numberofvillages = 0;
+            public Village nextvillage;
+            public Village previousvillage;
+            public string Villagename;
+            public bool isAstrildeHere = false;
+            public Village()
+            { Village.numberofvillages++; }
+        }
+        class Countryside
+        {
+            public Village Maple;
+
+            public Village Toronto;
+            public Village Ajax;
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.Villagename = "Maple";
+                Maple.previousvillage = null;
+                Toronto = new Village();
+                Toronto.Villagename = "Toronto";
+                Toronto.previousvillage = null;
+                Ajax = new Village();
+                Ajax.Villagename = "Ajax";
+                Ajax.nextvillage = null;
+                Ajax.previousvillage = Toronto;
+                Ajax.isAstrildeHere = true;
+
+
+            }
         }
     }
 }
